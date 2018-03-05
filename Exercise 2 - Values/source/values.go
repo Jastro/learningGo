2d8515ff9399bcc00af5dbd53a21103a45f4c09b
Jacobo <jacobo.caraballo@walmeric.com>
2018-03-05 11:58:31 +0100

    Created Exercises 1 to 3

---
+++ Exercise 2 - Values/source/values.go
@@ -0,0 +1,10 @@
+package main;
+
+import "fmt";
+
+func main(){
+	fmt.Println("Concatenando" + " textos");
+	fmt.Println("1+1 =", 1+1);
+	fmt.Println("5.0 / 2.5 = ", 5.0/2.5);
+	fmt.Println(!true);
+}
---
+++ Exercise 3 - Variables/source/variables.go
@@ -0,0 +1,20 @@
+package main;
+
+import "fmt";
+
+func main() {
+	var a = "inicio";
+	fmt.Println("a contiene: ", a);
+
+	var b,c int =  1,2;
+	fmt.Println(b,c);
+
+	var d = true;
+	fmt.Println("D contiene: ", d);
+
+	var e int
+	fmt.Println("E contiene", e);
+
+	f := "Prueba";
+	fmt.Println(f);
+}
